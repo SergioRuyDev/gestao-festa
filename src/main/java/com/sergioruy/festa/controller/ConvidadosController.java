b16647116ca23324ac7e17822fcc6727cfaec069
Sergio Ruy <sergioruyenator@gmail.com>
2020-04-12 16:53:43 +0400

    Adicionado convidados ao banco de dados e melhorias na view.

--- src/main/java/com/sergioruy/festa/controller/ConvidadosController.java
+++ src/main/java/com/sergioruy/festa/controller/ConvidadosController.java
@@ -15,7 +15,7 @@ public class ConvidadosController {
 
     @GetMapping("/convidados")
     public ModelAndView listar() {
-        ModelAndView modelAndView = new ModelAndView("ListaConvidados");
+        ModelAndView modelAndView = new ModelAndView("/ListaConvidados");
         modelAndView.addObject("convidados", convidadosRepository.findAll());
         return modelAndView;
     }
